eb7c3a0003ce4631eb12d0febc9252dbe9126c04
AnshumanTiware <67199135+AnshumanTiware@users.noreply.github.com>
2024-05-02 19:49:52 +0000

    Changed layout to a single column instead of 4 columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,49 +15,52 @@ st.code("pip install streamlit-image-coordinates")
 
 "Try clicking on any of the images below."
 
-col1, col2, col3, col4 = st.columns(4)
+# col1, col2, col3, col4 = st.columns(4)
+# col2, col1= st.columns(2)
+col2, col1= st.columns([0.000001, 0.9])
+
+# with col1:
+#     st.write("## Url example")
+
+#     with st.echo("below"):
+#         value = streamlit_image_coordinates(
+#             "https://placekitten.com/200/300",
+#             key="url",
+#         )
+
+#         st.write(value)
+
+# with col2:
+#     # st.write("## Local image example")
+
+#     # with st.echo("below"):
+#     #     value = streamlit_image_coordinates(
+#     #         "at_echo.jpeg",
+#     #         key="local",
+#     #     )
+
+#     #     st.write(value)
 
 with col1:
-    st.write("## Url example")
-
-    with st.echo("below"):
-        value = streamlit_image_coordinates(
-            "https://placekitten.com/200/300",
-            key="url",
-        )
-
-        st.write(value)
-
-with col2:
-    st.write("## Local image example")
-
-    with st.echo("below"):
-        value = streamlit_image_coordinates(
-            "kitty.jpeg",
-            key="local",
-        )
-
-        st.write(value)
-
-with col3:
     st.write("## Custom size example")
 
     with st.echo("below"):
         value = streamlit_image_coordinates(
-            "kitty.jpeg",
-            width=250,
+            "at_echo.jpeg",
+            height = 1024,
+            width=784,
             key="local2",
         )
 
         st.write(value)
 
-with col4:
-    st.write("## PIL example")
+# with col4:
+#     st.write("## PIL example")
 
-    with st.echo("below"):
-        value = streamlit_image_coordinates(
-            Image.open("kitty.jpeg"),
-            key="pil",
-        )
+#     with st.echo("below"):
+#         value = streamlit_image_coordinates(
+#             Image.open("kitty.jpeg"),
+#             key="pil",
+#         )
 
-        st.write(value)
+#         st.write(value)
